6b0b9e6200c72931cfbff3981aa3c89f67fa04fd
pavanchambhare <pavanchambhare88gmail.com>
2022-04-11 19:04:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+  .to(".water", 10, { y: -200 })
+  .to(".rocck", 10, { y: 600 }, "-=10")
+  .fromTo(".ok", { y: -500 }, { y: 0, duration: 10 }, "-=10")
+  .to(".content", 10, { top: "0%" }, "-=10")
+  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
+  .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
+
+let scene = new ScrollMagic.Scene({
+  triggerElement: "section",
+  duration: "300%",
+  triggerHook: 0,
+})
+  .setTween(timeline)
+  .setPin("section")
+  .addTo(controller)
